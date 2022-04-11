438e7373d61aa96e607ebdf6e21f5a9639dbe77d
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-04-11 20:31:21 +0300

    Лекция 3

---
+++ unit_one/lesson__1__06.04.2022/hometask/task1.py
@@ -0,0 +1,33 @@
+color = "RED"
+
+match color:
+    case "RED":
+        print("Флаг красный")
+    case "GREEN":
+        print("Трава зеленая")
+    case "BLUE":
+        print("Небо синее")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
