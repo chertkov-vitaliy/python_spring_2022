67d6cf1bfd12668b2bd1d0319ff119241a9e8f2e
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-04-15 20:24:12 +0300

    Работа в класе

--- unit_one/lesson__3__11.04.2022/classwork/task1.py
+++ unit_one/lesson__3__11.04.2022/classwork/task1.py
@@ -1,5 +1,16 @@
 # todo: Дано целое число. Если оно является положительным, то прибавить к нему 1;
 # если отрицательным, то вычесть из него 2; если нулевым, то заме# нить его на 10.
 # Вывести полученное число.
-lst = [3,4,5,3]
-lst.count(3)
+
+
+elements = {'el1': 1, 'el2': 0, 'el3': -2, 'el4': 95, 'el5': 13}
+for key in elements.items():
+    print(key)
+
+
+
+{ 'login': 'masha', 'email': 'masha@yandex.ru', 'password': 'fhei23jj~'}
+
+
+str = 'a', 'b', 'c'
+print(str)
