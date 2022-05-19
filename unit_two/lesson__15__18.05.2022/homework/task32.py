e91bf00a0dfdecc7839471de07ef74728abe71e6
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-05-19 09:08:21 +0300

    Д.З на 20 мая

---
+++ unit_two/lesson__15__18.05.2022/homework/task32.py
@@ -0,0 +1,8 @@
+# todo: Создать скрипт создания базы данных(ER-модели) TestSystem
+# Скрипт  create_db.py  должен создавать таблицы, индексы , констрейнты в СУБД PostgresSQL
+# В задании использовать библиотеку psycopg
+
+
+Ссылка на документацию
+https://www.psycopg.org/psycopg3/docs/basic/usage.html
+
