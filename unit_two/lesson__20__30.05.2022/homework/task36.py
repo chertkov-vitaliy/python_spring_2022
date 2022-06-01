973c9c1f292e88baa52a4379e0e5888994fd2acc
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-06-01 20:20:53 +0300

    Классная работа 1 июня

--- unit_two/lesson__20__30.05.2022/homework/task36.py
+++ unit_two/lesson__20__30.05.2022/homework/task36.py
@@ -3,3 +3,31 @@
 #  2. Сделать класс View абстрактным, а также метод render() абстрактным
 #  3. Реализовать  фабрику FabConsoleView в которой пораждаются экзепляры
 #     классов TestView, QuestionView и AuthView
+
+
+['__class__',
+ '__delattr__',
+ '__dict__',
+ '__dir__',
+ '__doc__',
+ '__eq__',
+ '__format__',
+ '__ge__',
+ '__getattribute__',
+ '__gt__',
+ '__hash__',
+ '__init__',
+ '__init_subclass__',
+ '__le__',
+ '__lt__',
+ '__module__',
+ '__ne__',
+ '__new__',
+ '__reduce__',
+ '__reduce_ex__',
+ '__repr__',
+ '__setattr__',
+ '__sizeof__',
+ '__str__',
+ '__subclasshook__',
+ '__weakref__']
