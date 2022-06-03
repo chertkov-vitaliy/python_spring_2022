99bc15913d7bcf476cdb29fe49c5a9b6a1c78134
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-06-03 20:01:05 +0300

    Классная работа 3 июня

---
+++ unit_two/lesson__22__03.06.2022/classwork/task.py
@@ -0,0 +1,21 @@
+# todo: 1 Реализовать итератор на базе класса Person.
+
+class Profile:
+    def __init__(self,  id, login,  password,  name,  age):
+        self.id = id
+        self.login = login
+        self.__password = password
+        self.name = name
+        self.age = age
+
+
+# Объект класса должен итерироваться циклом for
+obj = Profile( 1, "admin", 123456, "Peter", 25 )
+for i in obj:
+    # функция должна выводить название и значение атрибута
+    print(i)
+
+2 Реализовать генератор на базе класса Person. При итерировании циклом метод
+должен выводить название и значение атрибута
+
+3 Реализовать контекстный менеджер над DB . Класс должен возвращать соединение с БД
