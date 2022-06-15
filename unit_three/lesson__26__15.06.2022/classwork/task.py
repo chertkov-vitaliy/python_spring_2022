35dbf896e4d81ba5e4b59d8fba23365983a2be6b
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-06-15 20:33:47 +0300

    Классная работа 15 июня

---
+++ unit_three/lesson__26__15.06.2022/classwork/task.py
@@ -0,0 +1,32 @@
+# todo:
+0. Скачать http-clent https://insomnia.rest/download
+1. Создать новый проект и установить flask, flask-restx
+2. Создать файл requirements.txt  в который занести устанавливаемые библиотеки
+3. Написать роутеры для REST сервиса
+
+
+class Student(Resource):
+    def __init(self):
+        pass
+    #router /student/{id}
+    def get(self, id):
+        """ Получаем студента по его id """
+        pass
+    #router /student/{id}
+    def post(self):
+        """ Передаем изменения в json """
+
+    # router /student/
+    def put(self):
+        """ Добавить студента в json """
+
+    # router /student/{id}
+    def delete(self):
+        """ Удалим студента по {id}"""
+
+4. Реализовать роутер /student/list - получение списка всех студентов
+
+4. Добавить описание  для сваггера
+
+
+
