c8cf777dc4f2db38eb241bb09beceb3f1d0ad916
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-06-21 11:30:42 +0300

    Д.З к 22 июня

--- unit_three/lesson__28__20.06.2022/classwork/task.py
+++ unit_three/lesson__28__20.06.2022/classwork/task.py
@@ -1,2 +1,3 @@
-# todo: Создайте простое  приложение с помощи статьи:
+# todo: Создайте простое  приложение с помощью статьи:
+# Создание веб-приложения с помощью Flask в Python 3
 https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3-ru
---
+++ unit_three/lesson__28__20.06.2022/homework/task39.py
@@ -0,0 +1,3 @@
+# todo: Создайте простое  приложение с помощью статьи:
+# How to Use One-to-Many Database Relationships with Flask-SQLAlchemy
+https://www.digitalocean.com/community/tutorials/how-to-use-one-to-many-database-relationships-with-flask-sqlalchemy
