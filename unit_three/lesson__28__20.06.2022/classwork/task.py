6d29545596aca4fa6f451c2bc29a28964a9bbe35
chertkov-vitaliy <chertkov_vitaliy@mail.ru>
2022-06-22 20:10:53 +0300

    Классная работа 22 июня

--- unit_three/lesson__28__20.06.2022/classwork/task.py
+++ unit_three/lesson__28__20.06.2022/classwork/task.py
@@ -1,3 +1,4 @@
 # todo: Создайте простое  приложение с помощью статьи:
 # Создание веб-приложения с помощью Flask в Python 3
 https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3-ru
+Пренести туда REST из классного задания лекции 26 (lesson__26__15.06.2022)
